78e8c7613518e7fdb21bfa748dc89b1e6f7660e3
Jeff Frabell <jfrabell@yahoo.com>
2025-08-14 21:54:22 +0000

    try 458

---
+++ cognito-config.js
@@ -0,0 +1,14 @@
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+global.fetch = require('node-fetch'); // Needed if running Node.js outside the browser
+
+const poolData = {
+  UserPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID, // e.g. 'us-east-1_XXXXXX'
+  ClientId: process.env.REACT_APP_COGNITO_CLIENT_ID       // e.g. 'XXXXXXXXXXXX'
+};
+
+const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+module.exports = { userPool };
+
+REACT_APP_COGNITO_CLIENT_ID
+REACT_APP_COGNITO_USER_POOL_ID
